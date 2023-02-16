d32347913971e39ba777043814cb4e5482810866
alvinwendt <alvinwendt@gmail.com>
2023-02-15 21:16:06 -0600

    Initial Commit

---
+++ TaxiApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TaxiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sentiment Analysis:
+            Console.WriteLine("Write a review of your Lyft driver: ");
+
+            //Load input data
+            var sampleData = new ReviewSentimentModel.ModelInput()
+            {
+                Comment = Console.ReadLine()
+            };
+
+            //Load model and predict output
+            var result = ReviewSentimentModel.Predict(sampleData);
+
+            string printedString = "";
+
+            if (result.Prediction == "positive") { printedString = "Thanks for the great review! :)"; };
+
+            if (result.Prediction == "negative") { printedString = "We're sorry you had a bad experience. :("; };
+
+            Console.WriteLine($"\n{printedString}");
+        }
+    }
+}
